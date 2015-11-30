7bc0a1a98d3caefd3994f9d0dd4ec55850d95653
Khr0nos <antiwindows17@gmail.com>
2015-11-30 17:53:44 +0100

    Threaded

--- serverSocket.c
+++ serverSocket.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <signal.h>
+#include <pthread.h>
 
 int current_alive = 0;
 int limit = 0;
@@ -11,6 +12,8 @@ void trat_sigchld (int signum) {
     while (waitpid(-1, NULL, WNOHANG) > 0)
         --current_alive;
 }
+
+//--------------------------Sequencial---------------------------------------
 
 void doService(int fd) {
 int i = 0;
@@ -37,8 +40,9 @@ int socket_fd = (int) fd;
 	}
 	sprintf(buff2, "Server [%d] ends service\n", getpid());
 	write(1, buff2, strlen(buff2));
-
 }
+
+//--------------------------Unbounded---------------------------------------
 
 void doServiceFork(int fd) {
 	int pid = fork();
@@ -50,6 +54,8 @@ void doServiceFork(int fd) {
 			exit(0);
 	}
 }
+
+//--------------------------Bounded---------------------------------------
 
 void doServiceBounded(int fd) {
     int pid = fork();
@@ -64,6 +70,48 @@ void doServiceBounded(int fd) {
     if (current_alive > max) max = current_alive;
 }
 
+//--------------------------Threads---------------------------------------
+
+void *doThread(void *fd) {
+int i = 0;
+char buff[80];
+char buff2[80];
+int ret;
+int socket_fd = (int) fd;
+
+    ret = read(socket_fd, buff, sizeof(buff));
+    while(ret > 0) {
+        buff[ret]='\0';
+        sprintf(buff2, "Server [%d] received: %s\n", getpid(), buff);
+        write(1, buff2, strlen(buff2));
+        ret = write(fd, "caracola ", 8);
+        if (ret < 0) {
+            perror ("Error writing to socket");
+            pthread_exit(1);
+        }
+        ret = read(socket_fd, buff, sizeof(buff));
+    }
+    if (ret < 0) {
+            perror ("Error reading from socket");
+
+    }
+    sprintf(buff2, "Server [%d] ends service\n", getpid());
+    write(1, buff2, strlen(buff2));
+    pthread_exit(0);
+}
+
+void doServiceThreaded(int fd) {
+    pthread_t thread;
+    int ret = pthread_create(&thread, NULL, doThread, (void *)fd);
+    if (ret) {
+      perror("Error creating thread");
+      exit(-1);
+    }
+    ret = pthread_detach(thread);
+    if (ret) perror("Error detaching thread");
+}
+
+//--------------------------Main-----------------------------------------
 
 main (int argc, char *argv[])
 {
@@ -74,9 +122,9 @@ main (int argc, char *argv[])
   int port;
 
 
-  if (argc < 2)
+  if (argc != 2)
     {
-      strcpy (buffer, "Usage: ServerSocket PortNumber upper_bound\n");
+      strcpy (buffer, "Usage: ServerSocket PortNumber [upper_bound]\n");
       write (2, buffer, strlen (buffer));
       exit (1);
     }
@@ -93,7 +141,7 @@ main (int argc, char *argv[])
       exit (1);
     }
 
-  while (current_alive < limit) {   // (current_alive < limit) per a doServiceBounded
+  while (1) {   // (current_alive < limit) per a doServiceBounded
 	  connectionFD = acceptNewConnections (socketFD);
 	  if (connectionFD < 0)
 	  {
@@ -104,8 +152,10 @@ main (int argc, char *argv[])
 
 	  //doService(connectionFD);                //descomentar el que es vulgui provar
 	  //doServiceFork(connectionFD);
-      doServiceBounded(connectionFD);
+      //doServiceBounded(connectionFD);
+      doServiceThreaded(connectionFD);
   }
-  if (argc == 3) while (waitpid(-1, NULL, 0) > 0);
-  printf("Maximum children served: %d \n", max);
+  //if (argc == 3) while (waitpid(-1, NULL, 0) > 0);
+  //printf("Maximum children served: %d \n", max);
+  pthread_exit(0);
 }
